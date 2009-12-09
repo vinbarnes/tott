8a7a9140ff4e918f1be796bea6129b3c8ac0dbb0
Kevin R. Barnes <vinbarnes@gmail.com>
2009-12-09 10:25:05 -0600

    initial commit

---
+++ tott.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby -w
+
+DEBUG = true
+def log(msg) puts msg if DEBUG; end
+
+dir = (ARGV[0][0] == File::SEPARATOR[0]) ? ARGV[0] : File.expand_path(File.join(File.dirname(__FILE__), ARGV[0]))
+
+log "** Analyzing directory: " + dir
+
+Dir.chdir(dir)
+
+git_dirs = Dir['**/.git'].map {|d| d.sub('.git', '')}
+log "** git directories: "
+log git_dirs
+
+git_dirs.each do |git_dir|
+  log "** git gc starting in: #{git_dir}"
+  Dir.chdir(git_dir) do
+    log `git gc --aggressive`
+  end
+end
+
